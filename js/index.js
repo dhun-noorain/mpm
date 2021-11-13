a8b17b055838718aab043ca11a627cf5e8f8af8e
dhun-noorain <60414751+dhun-noorain@users.noreply.github.com>
2021-11-13 09:25:47 +0100

    Add index.js file

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  let typeInput = $('#type');
+  let priceInput = $('#price');
+  let quantityInput = $('#quantity');
+  let orderBtn = $('.btn-order');
+
+  orderBtn.click(function() {
+    quantityInput.val(1);
+    let values = $(this).val().split(' ');
+    let type = values[0];
+    let price = values[1];
+    typeInput.val(type);
+    priceInput.val('N'+price);
+  });
+
+  quantityInput.change(function() {
+    let cur_price = 0;
+    switch(typeInput.val()) {
+      case 'regular':
+        cur_price = quantityInput.val() * 200;
+        break;
+      case 'vip':
+        cur_price = quantityInput.val() * 250;
+        break;
+      case 'vvip':
+        cur_price = quantityInput.val() * 300;
+        break;
+    }
+    priceInput.val('N'+cur_price);
+  });
+});
