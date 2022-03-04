e0661a5b762113716cae94f18832b96c201a1ee1
Umar Faruk Haruna <harunaumar109@gmail.com>
2022-03-04 06:39:48 +0100

    Delete PHP directory

--- php/footer.php
+++
@@ -1,6 +0,0 @@
-  <script src="../js/all.js"></script>
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/bootstrap.bundle.min.js"></script>
-  <script src="../js/index.js"></script>
-</body>
-</html>
